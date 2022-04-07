60844c39e1d4206c52c9862017dd290fd5a20dc6
Sarah Indano <sarah.indano@gmail.com>
2022-04-07 14:47:02 -0400

    commit tree 0feadd37dd53f18f97e06ef634ba5702e4c8fa59

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :Students do |t|
+      t.string :name
+    end
   end
 end
---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,6 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[6.1]
+    def change
+        add_column :Students, :grade, :integer
+        add_column :Students, :birthdate, :string
+    end
+end
